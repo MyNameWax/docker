82988f0b098bedd95ccdf33ceabe69455386b0ff
mynamewax <waxjava04@163.com>
2025-05-24 17:23:48 +0800

    all

--- 数据检索服务/ES+kibina/elasticsearch/startup.sh
+++ 数据检索服务/ES+kibina/elasticsearch/startup.sh
@@ -1,4 +1,7 @@
 docker run -d --name my_elasticsearch \
 -p 9200:9200 -p 9300:9300 \
 -e discovery.type=single-node \
-my_elasticsearch
+elasticsearch:7.17.0
+
+
+docker run -d --name my_elasticsearch -p 9200:9200 -p 9300:9300 -e discovery.type=single-node elasticsearch:7.17.0
